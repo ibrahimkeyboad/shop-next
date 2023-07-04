425bbd9994695521b8c03f08fbfffab2117c2afb
ibrahimkeyboad <ibrahimmwanga07@gamil.com>
2023-07-04 13:11:03 +0300

    add seed products

--- src/app/api/seed/route.js
+++ src/app/api/seed/route.js
@@ -1,6 +1,7 @@
 import db from '@/db';
 import Product from '@/models/productModel';
 import User from '@/models/userModel';
+import { products } from '@/utils/product';
 import { users } from '@/utils/users';
 import { NextResponse } from 'next/server';
 
@@ -8,9 +9,20 @@ export async function GET(req) {
   try {
     await db.connect();
     await User.deleteMany();
-    await User.insertMany(users);
+
+    const createdUsers = await User.insertMany(users);
+
+    const adminUser = createdUsers[0]._id;
+
     await Product.deleteMany();
-    await Product.insertMany(data.products);
+
+    const sampleProducts = products.map((product) => {
+      return { user: adminUser, ...product };
+    });
+
+    console.log(sampleProducts);
+
+    await Product.insertMany(sampleProducts);
     return NextResponse.json({ message: 'seeded successfully' });
   } catch (err) {
     return new NextResponse(`${err}`, { status: 500 });
